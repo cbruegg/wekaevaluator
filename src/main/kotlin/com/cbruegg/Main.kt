e955344c854d2948eb3914b9e686b4beb2841c65
Christian <c.brueggemann94@gmail.com>
2017-01-14 21:15:58 +0100

    Add mode to check accuracy convergence

--- src/main/kotlin/com/cbruegg/Main.kt
+++ src/main/kotlin/com/cbruegg/Main.kt
@@ -17,16 +17,22 @@ import java.io.File
 import java.util.*
 import kotlin.concurrent.thread
 
-sealed class ValidateMode(val useFeatureSelection: Boolean, val useAllClassifiers: Boolean) {
-    class RandomCrossValidation(useFeatureSelection: Boolean, useAllClassifiers: Boolean) : ValidateMode(useFeatureSelection, useAllClassifiers)
-    class UserCrossValidation(useFeatureSelection: Boolean, useAllClassifiers: Boolean) : ValidateMode(useFeatureSelection, useAllClassifiers)
-    class PersonalRandomCrossValidation(useFeatureSelection: Boolean, useAllClassifiers: Boolean) : ValidateMode(useFeatureSelection, useAllClassifiers)
+sealed class ValidateMode(val useFeatureSelection: Boolean, val useAllClassifiers: Boolean, val evalConvergence: Boolean) {
+    class RandomCrossValidation(useFeatureSelection: Boolean, useAllClassifiers: Boolean, evalConvergence: Boolean) :
+            ValidateMode(useFeatureSelection, useAllClassifiers, evalConvergence)
+
+    class UserCrossValidation(useFeatureSelection: Boolean, useAllClassifiers: Boolean, evalConvergence: Boolean) :
+            ValidateMode(useFeatureSelection, useAllClassifiers, evalConvergence)
+
+    class PersonalRandomCrossValidation(useFeatureSelection: Boolean, useAllClassifiers: Boolean, evalConvergence: Boolean) :
+            ValidateMode(useFeatureSelection, useAllClassifiers, evalConvergence)
 }
 
 private const val FLAG_RANDOM_CROSS_VALIDATION = "--random-cross-validation"
 private const val FLAG_USE_FEATURE_SELECTION = "--feature-selection"
 private const val FLAG_USE_ALL_CLASSIFIERS = "--use-all-classifiers"
 private const val FLAG_PERSONAL_MODEL = "--personal"
+private const val FLAG_ACCURACY_CONVERGENCE = "--eval-convergence"
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
@@ -35,17 +41,19 @@ fun main(args: Array<String>) {
         println("$FLAG_USE_FEATURE_SELECTION will perform initial feature selection.")
         println("$FLAG_USE_ALL_CLASSIFIERS will use all classifiers instead of just RF.")
         println("$FLAG_PERSONAL_MODEL will perform cross-validation on models for one user only. Cannot be used in conjunction with $FLAG_RANDOM_CROSS_VALIDATION")
+        println("$FLAG_ACCURACY_CONVERGENCE will evaluate the model with 1 to N users.")
         return
     }
 
     val input = File(args[0])
     val useFeatureSelection = FLAG_USE_FEATURE_SELECTION in args
     val useAllClassifiers = FLAG_USE_ALL_CLASSIFIERS in args
+    val evalConvergence = FLAG_ACCURACY_CONVERGENCE in args
     val validateMode = if (FLAG_RANDOM_CROSS_VALIDATION in args) {
-        ValidateMode.RandomCrossValidation(useFeatureSelection, useAllClassifiers)
+        ValidateMode.RandomCrossValidation(useFeatureSelection, useAllClassifiers, evalConvergence)
     } else if (FLAG_PERSONAL_MODEL in args) {
-        ValidateMode.PersonalRandomCrossValidation(useFeatureSelection, useAllClassifiers)
-    } else ValidateMode.UserCrossValidation(useFeatureSelection, useAllClassifiers)
+        ValidateMode.PersonalRandomCrossValidation(useFeatureSelection, useAllClassifiers, evalConvergence)
+    } else ValidateMode.UserCrossValidation(useFeatureSelection, useAllClassifiers, evalConvergence)
 
     if (input.isDirectory) {
         val results = Collections.synchronizedMap(mutableMapOf<File, String>())
@@ -79,20 +87,20 @@ private fun evaluate(input: File, validateMode: ValidateMode): String {
         else baseModel
 
         threads += thread {
-            val data = loadDataFromFile(input)
+            val fullDataset = loadDataFromFile(input)
 
-            val usernameAttrIndex = data.attribute("username").index()
-            val users = data.attribute("username")
+            val usernameAttrIndex = fullDataset.attribute("username").index()
+            val users = fullDataset.attribute("username")
                     .enumerateValues()
                     .asSequence()
                     .distinct()
                     .filterIsInstance<String>()
                     .toList()
-            val userByInstance = data.associate {
+            val userByInstance = fullDataset.associate {
                 Arrays.hashCode(it.numericalValues()) to it.stringValue(usernameAttrIndex)
             }
 
-            val eval = when (validateMode) {
+            fun eval(data: Instances) = when (validateMode) {
                 is ValidateMode.PersonalRandomCrossValidation -> {
                     Evaluation(data).apply {
                         for (user in users) {
@@ -128,13 +136,20 @@ private fun evaluate(input: File, validateMode: ValidateMode): String {
                 }
             }
 
-            resultsByModel[description] = """
-            |+++ TRAINING $description +++
+            val reducedDatasets =
+                    if (validateMode.evalConvergence) generateSubsets(fullDataset, users, userByInstance)
+                    else sequenceOf(fullDataset)
+            val datasetsEvals = reducedDatasets.map(::eval)
+
+            resultsByModel[description] = datasetsEvals.mapIndexed { i, eval ->
+                """
+            |+++ TRAINING $description ${if (validateMode.evalConvergence) "with subset of size ${i + 1} " else ""}+++
             |=== Results of $description ===
             |${eval.toSummaryString("", false)}
             |=== Confusion Matrix of $description ===
             |${eval.toMatrixString("")}
             |""".trimMargin()
+            }.joinToString(separator = "\n")
         }
     }
     threads.forEach(Thread::join)
@@ -144,6 +159,28 @@ private fun evaluate(input: File, validateMode: ValidateMode): String {
         it.value
     }.joinToString(separator = "", prefix = "Now evaluating file $input.\n")
 }
+
+/**
+ * Generate subsets of the complete dataset to evaluate the convergence
+ * of the accuracy depending on the amount of data we have. This
+ * method will generate [Instances] with users {u1, u2}, ... {u1, ..., un}.
+ *
+ * @param [userByInstance] val instanceUser = userByInstance[Arrays.hashCode(instance.numericalValues())]
+ */
+fun generateSubsets(data: Instances, users: List<String>, userByInstance: Map<Int, String>) =
+        (2..users.size)
+                .asSequence()
+                .map { users.subList(0, it) }
+                .map { usersToKeep ->
+                    Instances(data).apply {
+                        for (i in indices.reversed()) {
+                            val instanceUser = userByInstance[Arrays.hashCode(this[i].numericalValues())]
+                            if (instanceUser !in usersToKeep) {
+                                removeAt(i)
+                            }
+                        }
+                    }
+                }
 
 fun Instance.numericalValues(): DoubleArray = (0 until numAttributes())
         .filter { attribute(it).isNumeric }
